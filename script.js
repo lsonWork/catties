ff669bea50031dc71f9a3199e7a5f3fd3723f4af
Le Son <lson999920.work@gmail.com>
2025-07-22 00:19:16 +0700

    fix formula

--- script.js
+++ script.js
@@ -29,26 +29,43 @@ copyBtn.addEventListener("click", () => {
 
 const btnCalculate = document.querySelector("#btnCalculate");
 const inputNDT = document.querySelector("#inputNDT");
-const resultVND = document.querySelector("#resultVND");
-const resultVNDAndShip = document.querySelector("#resultVNDAndShip");
-const resultRetail = document.querySelector("#resultRetail");
-const resulBase = document.querySelector("#resultBase");
+const noship = document.querySelector("#noship");
+const shipMaterial = document.querySelector("#shipMaterial");
+const interest = document.querySelector("#interest");
+const rose = document.querySelector("#rose");
+const retail = document.querySelector("#retail");
+const mkt = document.querySelector("#mkt");
 
 const calculateNDT = () => {
-  const res = Math.ceil(Number(inputNDT.value) * 3.7);
-  resultVND.textContent = res;
-  const resShip = Math.ceil(Number(inputNDT.value) * 3.7) + 15;
-  resultVNDAndShip.textContent = resShip;
-  const resBase = Math.ceil(Number((resShip / 100) * 30));
-  resulBase.textContent = resBase;
-  const resRetail = Math.ceil(resShip + 5 + resBase);
-  let final = 0;
-  if (resShip <= 200) {
-    final = resRetail + 30;
+  noship.textContent = Math.ceil(Number(inputNDT.value) * 3.7);
+  const shipMaterialValue = Math.ceil(Number(inputNDT.value) * 3.7) + 15 + 5;
+  shipMaterial.textContent = shipMaterialValue;
+
+  let estInterest = 0;
+  if (shipMaterialValue < 150) {
+    estInterest = 40;
   } else {
-    final = resRetail + (resRetail / 100) * 15;
+    estInterest = Math.ceil((shipMaterialValue / 100) * 30);
   }
-  resultRetail.textContent = final;
+  interest.textContent = estInterest;
+
+  const mktValue = Math.ceil((Number(shipMaterialValue) / 100) * 15);
+  mkt.textContent = mktValue;
+  // retail.textContent =
+  //   (Math.ceil(Number(inputNDT.value) * 3.7) +
+  //     15 +
+  //     5 +
+  //     estInterest +
+  //     Math.ceil((Number(shipMaterialValue) / 100) * 15)) /
+  //   (1 - 0.3);
+
+  retail.textContent =
+    (Math.ceil(Number(inputNDT.value) * 3.7) +
+      15 +
+      5 +
+      estInterest +
+      mktValue) /
+    (1 - 0.3);
 };
 
 window.addEventListener("keydown", (e) => {
